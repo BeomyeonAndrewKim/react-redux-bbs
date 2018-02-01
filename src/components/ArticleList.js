1af58c969d76288c9b0f64a3f1811c16511271d9
BeomyeonAndrewKim <beom911@gmail.com>
2018-02-01 12:35:25 +0900

    Revise ArticleList component
    Simplified Presentation Component of ArticleList Component

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react';
 import { List } from 'semantic-ui-react';
 
-const articles = [
-  {
-    id: 'article0',
-    title: '게시글 제목',
-    createdAt: 1517453830448,
-    nickName: '김범연',
-  },
-  {
-    id: 'article1',
-    title: '게시글 제목2',
-    createdAt: 1517453912252,
-    nickName: '김미연',
-  },
-];
 
 export default class ArticleList extends Component {
-  state = {};
+  static defaultProps ={
+    articles: [],
+  }
   render() {
+    const { articles } = this.props;
     return (
       <List divided relaxed>
         {articles.map(({
-           id, title, createdAt, nickName,
+           id, title, createdAt, nickName, itemProps = {},
           }) => (
-            <List.Item key={id}>
+            <List.Item key={id} {...itemProps}>
               <List.Icon name="github" size="large" verticalAlign="middle" />
               <List.Content>
-                <List.Header as="a">
+                <List.Header>
                   {title} ({nickName})
                 </List.Header>
-                <List.Description as="a">{createdAt}</List.Description>
+                <List.Description>{createdAt}</List.Description>
               </List.Content>
             </List.Item>
         ))}
--- stories/ArticleList.js
+++ stories/ArticleList.js
@@ -3,4 +3,30 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import ArticleList from '../src/components/ArticleList';
 
-storiesOf('ArticleList', module).add('default', () => <ArticleList />);
+
+const articles = [
+  {
+    id: 'article0',
+    title: '게시글 제목',
+    createdAt: 1517453830448,
+    nickName: '김범연',
+  },
+  {
+    id: 'article1',
+    title: '게시글 제목2',
+    createdAt: 1517453912252,
+    nickName: '김미연',
+  },
+];
+
+const articelsWithLink = articles.map(article => ({
+  ...article,
+  itemProps: {
+    as: 'a',
+    href: 'https://google.com',
+  },
+}));
+
+storiesOf('ArticleList', module)
+  .add('default', () => <ArticleList articles={articles} />)
+  .add('links', () => <ArticleList articles={articelsWithLink} />);
