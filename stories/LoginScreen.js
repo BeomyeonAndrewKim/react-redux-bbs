468f0ef093e7ccfefdccf55d90cba5592d4dbcd8
BeomyeonAndrewKim <beom911@gmail.com>
2018-02-01 11:41:04 +0900

    Revise .eslintrc setting

--- stories/LoginScreen.js
+++ stories/LoginScreen.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import LoginScreen from '../src/components/LoginScreen';
 
-storiesOf('LoginScreen', module)
-  .add('default', () => <LoginScreen onGoogleLogin={action('onGoogleLogin')} />)
-  .add('dafault', () => <LoginScreen />);
+storiesOf('LoginScreen', module).add('default', () => (
+  <LoginScreen onGoogleLogin={action('onGoogleLogin')} />
+));
